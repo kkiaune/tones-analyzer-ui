46617cf19d798ae5efb2e16b8cb822f4d5908c23
Karolis Kiaune <karoliskiaune@Karoliss-MacBook-Pro.local>
2019-05-09 13:54:52 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
+import App from './App';
+
+library.add(faStroopwafel)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
