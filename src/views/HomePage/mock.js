f9c1589c36dc4a239595dd084db86c82eb2a791a
Karolis Kiaune <karoliskiaune@Karoliss-MacBook-Pro.local>
2019-05-09 16:45:09 +0300

    Moved collapsed drawer items to other component

---
+++ src/views/HomePage/mock.js
@@ -0,0 +1,17 @@
+export const availableChatsMock = [
+    {
+        id: 'chat1',
+        icon: 'file',
+        summary: 'Chat 1'
+    },
+    {
+        id: 'chat2',
+        icon: 'file',
+        summary: 'Chat 2'
+    },
+    {
+        id: 'chat3',
+        icon: 'file',
+        summary: 'Chat 3'
+    }
+];
